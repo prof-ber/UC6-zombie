c260c9d3a96cb9fc092043d6e8987f7e11e752af
Pedro <pedroarmiliato2006@gmail.com>
2024-06-21 17:43:19 -0300

    Merge branch 'main' of https://github.com/prof-ber/UC6-zombie

--- Player.gd
+++ Player.gd
@@ -1,7 +1,9 @@
 extends CharacterBody2D
 
+@export var vida = 100
 var motion = Vector2()
 var speed = 256
+var morto = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,20 +11,30 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	motion = Vector2(0,0)
-	speed = 256
-	self.get_child(1).look_at(get_global_mouse_position())
-	if (Input.is_action_pressed("Up")):
-		motion.y = -1
-	elif (Input.is_action_pressed("Down")):
-		motion.y = 1
-	if (Input.is_action_pressed("Left")):
-		motion.x = -1
-	elif (Input.is_action_pressed("Right")):
-		motion.x = 1
-	if (Input.is_action_pressed("Run")):
-		speed *= 2
-	if (Input.is_action_pressed("Shoot")):
-		self.get_child(1).fire()
-	self.velocity = motion * speed
-	move_and_slide()
+	if not morto:
+		motion = Vector2(0,0)
+		speed = 256
+		self.get_child(1).look_at(get_global_mouse_position())
+		if (Input.is_action_pressed("Up")):
+			motion.y = -1
+		elif (Input.is_action_pressed("Down")):
+			motion.y = 1
+		if (Input.is_action_pressed("Left")):
+			motion.x = -1
+		elif (Input.is_action_pressed("Right")):
+			motion.x = 1
+		if (Input.is_action_pressed("Run")):
+			speed *= 2
+		if (Input.is_action_pressed("Shoot")):
+			self.get_child(1).fire()
+		self.velocity = motion * speed
+		move_and_slide()
+
+func tomar_dano(quantidade):
+	self.vida -= quantidade
+	if self.vida < 1:
+		self.morrer()
+
+func morrer():
+	print("morri!")
+	self.morto = true
