e59e97580008d3f5c2ef64afb91bc8a163901024
Eminen <eminemarque@gmail.com>
2024-06-28 16:56:11 -0300

    Coisas de zumbi

---
+++ SpawnZombie.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _on_timer_timeout():
+	##Criar um zumbi
+	var new_zombie = Preloader.zombie.instantiate()
+	##Configurar a posição inicial do zumbi
+	new_zombie.global_position = self.global_position
+	##Adicionar o zumbi ao jogo
+	get_tree().get_root().get_child(1).add_child(new_zombie)
